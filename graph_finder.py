426b2bd361181de65d55e8bf116704b68569a258
AB laptoop <axbra047@gmail.com>
2021-06-02 22:11:29 +0200

    Generalizing joinadjacentneighbors
    Just before generalizing a part of joining neighbors adjazent to eachother and removing comments, saved if problems occur

--- graph_finder.py
+++ graph_finder.py
@@ -38,41 +38,93 @@ def getNonMaskedNeighbors(px,i_xy,maskArray):
 
     return A
 
+def joinAdjacentNeighbors(px,root, neighbors, maskArray,varnings=True):
+    # 1.42 is an upperbound to math.sqrt(2) and faster to use computationaly
+    nL = len(neighbors)
+    if nL == 2 and math.dist(neighbors[0], neighbors[1]) <= 1.42:
+        n1, n2 = neighbors
+        # masked because part of same vertex as root is
+        maskArray[n1[1], n1[0]] = maskArray[n2[1], n2[0]] = ma.masked
+        neighbors = set(getNonMaskedNeighbors(px, n1, maskArray)).union(getNonMaskedNeighbors(px, n2, maskArray))
+        if varnings and len(neighbors) >= 3:
+            print("Varning, at (x,y)=({},{}), 4 edges neighboring a vertex, output will be incorrect.".format(root[0], root[1]))
+    elif nL == 3:
+        n = neighbors
+        # n1, n2, n3 = neighbors
+        # adjacency = [math.dist(n1, n2) <= 1.42, math.dist(n1, n3) <= 1.42, math.dist(n2, n3) <= 1.42]
+        adjacent  = set()
+
+        # Adds neighbors with adjacency relation together into the set: adjacent
+        for i in range(nL-1):
+            for j in range(i+1,nL):
+                if math.dist(n[i],n[j]) <= 1.42:
+                    adjacent.union([n[i],n[j]])
+                    maskArray[n[i][1], n[i][0]] = maskArray[n[j][1], n[j][0]] = ma.masked
+
+        neighbors = set(neighbors).difference(adjacent)
+        for e in adjacent:
+            neighbors = neighbors.union(getNonMaskedNeighbors(px, e, maskArray))
+        # if sum(adjacency) == 0:
+        #     # case 1: no neighbor is adjacent => just pass past the if-statement
+        #     pass
+        # if sum(adjacency) == 1:
+        #     # case 2: two neighbors are adjacent, one is not
+        #     adPair = adjacency.index(True)
+        #     if adPair == 0:
+        #         # n1,n2
+        #         maskArray[n1[1], n1[0]] = maskArray[n2[1], n2[0]] = ma.masked
+        #         neighbors = set(getNonMaskedNeighbors(px, n1, maskArray)).union(
+        #             getNonMaskedNeighbors(px, n2, maskArray)).add(n3)
+        #     elif adPair == 1:
+        #         # n1,n3
+        #         maskArray[n1[1], n1[0]] = maskArray[n3[1], n3[0]] = ma.masked
+        #         neighbors = set(getNonMaskedNeighbors(px, n1, maskArray)).union(
+        #             getNonMaskedNeighbors(px, n3, maskArray)).add(n2)
+        #     elif adPair == 2:
+        #         # n2,n3
+        #         maskArray[n2[1], n2[0]] = maskArray[n3[1], n3[0]] = ma.masked
+        #         neighbors = set(getNonMaskedNeighbors(px, n2, maskArray)).union(
+        #             getNonMaskedNeighbors(px, n3, maskArray)).add(n1)
+        # elif sum(adjacency) in [2, 3]:
+        #     # case 3: all three neighbors are adjacent or all are adjacent to one of them
+        #     maskArray[n1[1], n1[0]] = maskArray[n2[1], n2[0]] = maskArray[n3[1], n3[0]] = ma.masked
+        #     neighbors = set(getNonMaskedNeighbors(px, n1, maskArray)).union(
+        #         getNonMaskedNeighbors(px, n2, maskArray)).union(getNonMaskedNeighbors(px, n3, maskArray))
+        #     # todo if new neighbors are adjacent, join with root (solution to recursion)
+
+            # Todo ta bort?
+            if varnings and len(neighbors) >= 3:
+                print("Varning(3.1), at (x,y)=({},{}), 4 edges neighboring a vertex, output will be incorrect.".format(x,y))
+    return neighbors
 
 def getNonArcPixels(px,nonArcPixels,root,maskArray, varnings=True):
 
     (x,y) = root
-    # if parent == None: # todo lägg till detta
-    #     parent = root
-    #todo för nonArcPixels bygg en (Parent,child) lista typ [((1,1),(50,5)), ...]
     while True:
-        # px[root] = off #Todo ta bort detta men se till så den inte går bakåt
         maskArray[y,x] = ma.masked
-        # Todo Tar just nu neighbors i ordning höger till vänster när nedanför
         neighbors = getNonMaskedNeighbors(px,root,maskArray)
-        # if neighbor is non-arc pixel. An arc-pixel is a pixel between intersections or endpoints
         nL = len(neighbors)
-        if  nL == 1:
-            # If pixel is an arc-pixel => neighbors only contain 1 element => first element is the only element
-            (x,y) = root = neighbors[0]
-        else:
-            # nL == 0 or nL == 2
-            nonArcPixels.append(root) #todo ta bort
-            # End Point
-            if nL == 0:
-                return nonArcPixels
-            # Handles problems with cases too many neighbors
-            elif varnings and nL >= 3: print("Varning, at (x,y)=({},{}), 4 edges neighboring a vertex, output will be incorrect.".format(x,y))
-            # elif nL in (2,3):
-            for neighbor in neighbors:
-                (xn,yn) = neighbor
-                if varnings and ma.is_masked(maskArray[yn,xn]):
-                    print("Varning, at (x,y)=({},{}), Cycle appears in the graph, output will be incorrect.".format(xn,yn))
-                nonArcPixels.extend(getNonArcPixels(px,[], neighbor,maskArray))
 
+        # Arcpixel, START===========
+        if  nL == 1: # Arc Pixel, should be ignored
+            (x,y) = root = neighbors[0]
+            continue
+
+        nonArcPixels.append(root)
+        if nL == 0:  # End Point
             return nonArcPixels
-        # todo lägga till vid större korsningar:
-        #  else: raise Exception("Too many neighbors when the number is {}, something is wrong at pixel {}".format(nL,root))
+        elif nL >= 2:  # May be an intersection,
+            # nL should be 2<=x<=5
+            neighbors = joinAdjacentNeighbors(px,root, neighbors, maskArray)
+
+
+        for neighbor in neighbors:
+            (xn, yn) = neighbor
+            if varnings and ma.is_masked(maskArray[yn, xn]):
+                print("Varning, at (x,y)=({},{}), Cycle appears in the graph, output will be incorrect.".format(xn, yn))
+            nonArcPixels.extend(getNonArcPixels(px, [], neighbor, maskArray))
+
+        return nonArcPixels
 
 
 
