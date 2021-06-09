954398d68bd8bd6e166ad5a2106dfca5aff3e3f6
AB laptop <axbra047@gmail.com>
2021-06-09 13:39:47 +0200

    Prototype Graph generator

--- graph_generation.py
+++ graph_generation.py
@@ -20,5 +20,50 @@ def treePlotter(newick):
     t = Tree(newick, format=8)
     # Tell ETE to use your custom Tree Style
     t.show(tree_style=ts)
-#     test
 
+from itertools import product
+# temp buffer to prevent infinite loop in for worOn..., defined here for risk of undefined after first loop
+newList = set()
+# L will have all trees in the end
+L = {"L","(L,L)"}
+# Buffer for trees added in the last insert iteration
+lastAddedToL = {"(L,L)"}
+# iteration index for times smaller trees are inserted into trees in lastAddedToL
+for insert in range(6):
+    newList = set()
+    # clears buffer lastAddedToL by iterating on all elements in it
+    for workOn in lastAddedToL:
+        # workOn split into non-leaf components
+        holes = workOn.split("L")
+        # list of different substitutions to fill the "holes" in holes with
+        substitutions = list(product(L, repeat=len(holes)-1))
+        for e in substitutions:
+            e = list(e)
+            # To make lists same length
+            e.append("")
+            # replaces holes
+            k = "".join([str(x) + str(y) for x, y in zip(holes, e)])
+            newList.add(k)
+
+    print(insert)
+    L.union(lastAddedToL)
+    lastAddedToL = newList
+    # newList = []
+L = L.union(lastAddedToL)
+L = L.difference({"L", "(L,L)"})
+
+L2 = set()
+for e in L:
+    # L.discard(e)
+    L2.add(e+";")
+
+print(L2)
+
+#
+
+
+
+
+# if __name__ == "__main__":
+#     leaves = 3
+#     print(balancedTree(leaves))
